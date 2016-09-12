71f1add1eefe6db002577cccdec809be4694f552
Dmitriy Pavlov <dpavlov@smartling.com>
2016-09-12 18:03:15 +0300

    updated cron tasks in cron.pp

--- puppet/cron.pp
+++ puppet/cron.pp
@@ -1,53 +1,53 @@
 cron { 'cron1':
-  command => '/bin/sh /path/to/push_to_gecko.sh',
+  command => '/bin/sh /custom/scripts/script1.sh',
   minute  => '*/15',
 }
 
 cron { 'cron2':
-  command => '/bin/sh /path/to/check_new_users.sh dbs base',
+  command => '/bin/sh /custom/scripts/script2.sh',
   minute  => 1,
   hour    => 12,
 }
 
 cron { 'cron3':
-  command => '/bin/sh /path/to/tests_stats.sh',
+  command => '/bin/sh /custom/scripts/script3.sh',
   minute  => 10,
   hour    => 0,
 }
 
 cron { 'cron4':
-  command => '/bin/sh /path/to/tests_stats_weekly.sh',
+  command => '/bin/sh /custom/scripts/script4.sh',
   minute  => 0,
 }
 
 cron { 'cron5':
-  command => '/bin/sh /path/to/repos_stats.sh',
+  command => '/bin/sh /custom/scripts/script5.sh',
   minute  => 0,
 }
 
 cron { 'cron6':
-  command => '/bin/sh /path/to/send_emails_to_d.sh',
+  command => '/bin/sh /custom/scripts/script6.sh',
   minute  => 50,
   hour    => 23,
   weekday => 7,
 }
 
 cron { 'cron7':
-  command => '/bin/sh /path/to/create_daily_job.sh',
+  command => '/bin/sh /custom/scripts/script7.sh',
   minute  => 0,
   hour    => 10,
   weekday => [1, 2, 3, 4, 5],
 }
 
 cron { 'cron8':
-  command => '/bin/sh /path/to/create_d_job.sh',
+  command => '/bin/sh /custom/scripts/script8.sh',
   minute  => 0,
   hour    => [13, 21],
   weekday => [1, 2, 3, 4, 5],
 }
 
 cron { 'cron9':
-  command => '/bin/sh /path/to/synch_all.sh dbm',
+  command => '/bin/sh /custom/scripts/script9.sh',
   minute  => 0,
   hour    => [12, 0],
 }
